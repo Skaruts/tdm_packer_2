bbc968e52d9c78b651ac2dd7d541127efa38d202
Skaruts <Skaruts@users.noreply.github.com>
2025-06-21 17:04:26 +0100

    add right-click menu to missions list

--- scenes/gui/gui_missions_list.gd
+++ scenes/gui/gui_missions_list.gd
@@ -1,15 +1,24 @@
 class_name GuiMissionsList
 extends TabContainer
 
-@onready var btn_open_mission: Button = %btn_open_mission
-@onready var btn_close_mission: Button = %btn_close_mission
 
-@onready var btn_play_mission: Button = %btn_play_mission
-@onready var btn_run_dr: Button = %btn_run_dr
-@onready var btn_pack_mission: Button = %btn_pack_mission
-@onready var btn_test_pack: Button = %btn_test_pack
+enum ListMenu {
+	OPEN_FOLDER,
+	OPEN_TEST_FOLDER,
+	CLOSE,
+}
 
-@onready var itemlist_missions: ItemList = %ilist_missions
+
+@onready var btn_open_mission  : Button = %btn_open_mission
+@onready var btn_close_mission : Button = %btn_close_mission
+
+@onready var btn_play_mission  : Button = %btn_play_mission
+@onready var btn_run_dr        : Button = %btn_run_dr
+@onready var btn_pack_mission  : Button = %btn_pack_mission
+@onready var btn_test_pack     : Button = %btn_test_pack
+
+@onready var il_missions       : ItemList  = %il_missions
+@onready var pu_missions_menu  : PopupMenu = %missions_popup_menu
 
 
 
@@ -23,12 +32,78 @@ func _ready() -> void:
 	launcher.tdm_copy_process_started.connect(func() -> void: btn_test_pack.disabled = true)
 	launcher.tdm_copy_process_ended.connect(func() -> void: btn_test_pack.disabled = false)
 
+	var menu:PopupMenu = pu_missions_menu
+	menu.add_item("Open Folder",      ListMenu.OPEN_FOLDER)
+	menu.add_item("Open Test Folder", ListMenu.OPEN_TEST_FOLDER)
+	menu.add_separator()
+	menu.add_item("Close",            ListMenu.CLOSE)
 
-func initialize() -> void:
+	pu_missions_menu.id_pressed.connect(_on_pu_missions_menu_id_pressed)
+	il_missions.item_clicked.connect(_on_il_missions_item_clicked)
 	btn_open_mission.pressed.connect(_on_btn_open_mission_pressed)
 	btn_close_mission.pressed.connect(_on_btn_close_mission_pressed)
-	itemlist_missions.item_selected.connect(_on_item_selected)
+
+
+func initialize() -> void:
 	update_list()
+
+
+func update_list() -> void:
+	il_missions.clear()
+	for m:Mission in fms.missions:
+		var id := m.id
+		var idx := il_missions.add_item(id)
+
+		#if m.file_tree == null:
+			#il_missions.set_item_custom_fg_color(idx, data.ERROR_COLOR)
+			#il_missions.set_item_metadata(idx, "invalid mission")
+
+	if fms.missions.size() > 0:
+		#logs.print("curr_idx: ", fms.get_current_mission_index())
+		il_missions.select(fms.get_current_mission_index())
+
+	update_buttons()
+
+
+func update_buttons() -> void:
+	var no_missions: bool = fms.missions.size() == 0
+	btn_open_mission.disabled  = not data.is_tdm_path_set()
+	btn_close_mission.disabled = no_missions
+
+	if not no_missions and fms.curr_mission.file_tree != null:
+		btn_play_mission.disabled     = not data.is_tdm_path_set()
+		btn_run_dr.disabled           = not data.is_dr_path_set()
+		btn_pack_mission.disabled     = not data.is_tdm_path_set()
+		btn_test_pack.disabled        = not data.is_tdm_copy_path_set() or not fms.is_mission_packed(fms.curr_mission)
+	else:
+		btn_play_mission.disabled     = true
+		btn_run_dr.disabled           = true
+		btn_pack_mission.disabled     = true
+		btn_test_pack.disabled        = true
+
+
+
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
+
+#        signals
+
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
+func _on_il_missions_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
+	if mouse_button_index > 2:
+		return
+
+	fms.select_mission(index)
+
+	if mouse_button_index == 2:
+		pu_missions_menu.position = get_global_mouse_position()
+		pu_missions_menu.popup()
+
+
+func _on_pu_missions_menu_id_pressed(id:int) -> void:
+	match id:
+		ListMenu.OPEN_FOLDER:      launcher.open_mission_folder()
+		ListMenu.OPEN_TEST_FOLDER: launcher.open_mission_test_folder()
+		ListMenu.CLOSE:            _on_btn_close_mission_pressed()
 
 
 func _on_btn_open_mission_pressed() -> void:
@@ -52,55 +127,10 @@ func _on_btn_open_mission_pressed() -> void:
 
 
 func _on_btn_close_mission_pressed() -> void:
+	# TODO: maybe ask confirmation?
 	if fms.is_save_timer_counting():
 		fms.stop_timer_and_save()
 	fms.remove_current_mission()
-
-
-func update_buttons() -> void:
-	var no_missions: bool = fms.missions.size() == 0
-	btn_open_mission.disabled  = not data.is_tdm_path_set()
-	btn_close_mission.disabled = no_missions
-
-	if not no_missions and fms.curr_mission.file_tree != null:
-		btn_play_mission.disabled     = not data.is_tdm_path_set()
-		btn_run_dr.disabled           = not data.is_dr_path_set()
-		btn_pack_mission.disabled     = not data.is_tdm_path_set()
-		btn_test_pack.disabled        = not data.is_tdm_copy_path_set() or not fms.is_mission_packed(fms.curr_mission)
-	else:
-		btn_play_mission.disabled     = true
-		btn_run_dr.disabled           = true
-		btn_pack_mission.disabled     = true
-		btn_test_pack.disabled        = true
-
-
-func update_list() -> void:
-	itemlist_missions.clear()
-	for m:Mission in fms.missions:
-		var id := m.id
-		var idx := itemlist_missions.add_item(id)
-
-		#if m.file_tree == null:
-			#itemlist_missions.set_item_custom_fg_color(idx, data.ERROR_COLOR)
-			#itemlist_missions.set_item_metadata(idx, "invalid mission")
-
-	if fms.missions.size() > 0:
-		#logs.print("curr_idx: ", fms.get_current_mission_index())
-		itemlist_missions.select(fms.get_current_mission_index())
-
-	update_buttons()
-
-
-func _on_item_selected(idx:int) -> void:
-	fms.select_mission(idx)
-
-
-#func update_current_mission_id() -> void:
-	#var idx := fms.get_current_mission_index()
-	#var id := fms.curr_mission.id
-	#if fms.curr_mission.dirty:
-		#id += " (*)"
-	#itemlist_missions.set_item_text(idx, id)
 
 
 func _on_btn_play_mission_pressed() -> void:
